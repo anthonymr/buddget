cf3d52ce0608414e7e309d33bd189016a594570f
Anthony Martin <anthonymartin20@gmail.com>
2023-05-29 10:09:06 -0400

    Add total amount to  categories

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,8 @@ class Group < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :author_id, presence: true
   validates :icon, presence: true
+
+  def total_amount
+    payments.where(author_id:).sum(:amount)
+  end
 end
